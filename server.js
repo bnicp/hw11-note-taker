ed53062b1efdf77e3cd2abd044d410eccd55155a
bnicp <bnp415@gmail.com>
2022-05-16 15:54:16 -0400

    added routes, js files, set up ports, and included get/post

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiroutes');
 
 //Initialize the app and create a port
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up the Express app to handle data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// API routes - load before HTML routes -- those catch everything else
+app.post('/api', (req, res) => {
+    res.json('post');
+});
+
+app.use('/', apiRoutes);
+
+// HTML routes
+app.use('/', htmlRoutes);
+
+
+
+// Start the server on the port
+app.listen(PORT, () => console.log(`Express server listening on port ${PORT}!`));
 
