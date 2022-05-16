b54185538cba61fd902b19d5e6d10342a9a5b9b5
bnicp <bnp415@gmail.com>
2022-05-16 17:32:26 -0400

    able to add/retrieve notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
 
 router.get('/notes', (req, res) => {
-    res.json('get!');
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 router.post('/notes', (req, res) => {
-    res.json('post!');
+    const newNote = req.body
+    newNote.id = uuidv4();
+
+    readAndAppend(newNote, './db/db.json')
+    res.json(newNote);
 });
 
 module.exports = router;
